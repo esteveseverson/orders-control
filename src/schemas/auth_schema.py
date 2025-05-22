2b0fc199046cf522acbcfe88292d789e5541dc24
everson <esteveseverson@gmail.com>
2025-05-22 07:23:22 -0300

    feat: create auth user schema

---
+++ src/schemas/auth_schema.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from pydantic import BaseModel, EmailStr
+
+from src.models.auth_model import UserProfile
+
+
+class BaseUser(BaseModel):
+    name: str
+    email: EmailStr
+    profile: UserProfile
+
+
+class CreateUser(BaseUser):
+    password: str
+
+
+class PublicUser(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+
+class ListUser(BaseModel):
+    users: List[PublicUser]
